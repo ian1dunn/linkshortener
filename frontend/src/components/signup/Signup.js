7d710336b6682078cb9899e5a796e58603efd056
Ian Dunn <itd3516@rit.edu>
2024-08-21 18:27:44 -0400

    Setup login and signup forms

--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,11 +1,112 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+  Form,
+  FormControl
+} from "react-bootstrap";
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: "",
+      last_name: "",
+      username: "",
+      email: "",
+      password: ""
+    };
+  }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSignupClick = () => {
+    const userData = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log("Sign up " + userData.first_name + " " + userData.last_name + " " + userData.username + " " + userData.email + " " + userData.password);
+  };
+
   render() {
     return (
       <Container>
-        <h1>Signup</h1>
+        <Row>
+          <Col md="4">
+            <h1>Sign Up</h1>
+            <Form>
+              <Form.Group controlId="nameId">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="first_name"
+                  placeholder="Enter First Name"
+                  value={this.state.first_name}
+                  onChange={this.onChange}
+                />
+                <Form.Control
+                  type="text"
+                  name="last_name"
+                  placeholder="Enter Last Name"
+                  value={this.state.last_name}
+                  onChange={this.onChange}
+                />
+                <FormControl.Feedback type="invalid"></FormControl.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="usernameId">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="username"
+                  placeholder="Enter Username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                />
+                <FormControl.Feedback type="invalid"></FormControl.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="emailId">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="email"
+                  placeholder="Enter Email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+                <FormControl.Feedback type="invalid"></FormControl.Feedback>
+              </Form.Group>
+
+
+              <Form.Group controlId="passwordId">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Enter Password"
+                  value={this.password}
+                  onChange={this.onChange}
+                />
+                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+              </Form.Group>
+            </Form>
+            <Button
+              color="primary"
+              onClick={this.onSignupClick}
+            >Sign up</Button>
+            <p className="mt-2">
+              Already have account? <Link to="/login">Login</Link>
+            </p>
+          </Col>
+        </Row>
       </Container>
     );
   }
