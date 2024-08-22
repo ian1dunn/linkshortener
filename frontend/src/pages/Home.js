c729f4f96c2ee20417b52d226e5277c98517d375
Ian Dunn <itd3516@rit.edu>
2024-08-22 02:40:32 -0400

    Setup basic homepage API request

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,36 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import AuthContext from "../context/AuthContext";
 
 const HomePage = () => {
-    const { user } = useContext(AuthContext);
+    const { authTokens, logoutUser } = useContext(AuthContext);
+    let [links, setLinks] = useState([])
+
+    useEffect(() => {
+        getLinks()
+    },[])
+
+    const getLinks = async() => {
+        let response = await fetch('http://127.0.0.1:8000/api/links/', {
+        method: 'GET',
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization':'Bearer ' + String(authTokens.access)
+        }
+        })
+        let data = await response.json()
+        console.log(data)
+        if(response.status === 200){
+            setLinks(data)
+        } else if(response.statusText === 'Unauthorized'){
+            logoutUser()
+        }
+    }
 
     return (
-        user ? (
         <div>
             <p>You are logged in to the homepage!</p>
+            <p>{links.toString()}</p>
         </div>
-        ):(
-        <div>
-            <p>You are not logged in, redirecting...</p>
-        </div>
-        )
     )
 }
 
