a8bb0a3b525066d68e82ec9d69a6527e71ac968d
Ian Dunn <itd3516@rit.edu>
2024-08-22 01:47:52 -0400

    Setup providing auth context to all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,19 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Header from './components/Header'
 import PrivateRoute from "./utils/PrivateRoute";
+import {AuthProvider} from "./context/AuthContext";
 
 function App() {
     return (
         <div className="App">
             <Router>
-                <Header/>
+                <AuthProvider>
+                    <Header/>
                 <Routes>
                     <Route path="/" element={<PrivateRoute><Home/></PrivateRoute>} />
                     <Route path="/login" element={<Login/>}/>
                 </Routes>
+                </AuthProvider>
             </Router>
         </div>
     );
