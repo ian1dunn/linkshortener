df726c27ea15a65b0b875c53aff99d02249c8644
Ian Dunn <itd3516@rit.edu>
2024-09-04 19:07:05 -0400

    Change root url to be dynamic

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export let ROOT_URL = 'http://127.0.0.1:8000/api';
+export let ROOT_URL = `${window.location.protocol}//${window.location.hostname}:8000/api`;
 export let TOKEN_ENDPOINT = '/token/';
 export let TOKEN_REFRESH_ENDPOINT = TOKEN_ENDPOINT + 'refresh/';
 export let LINK_ENDPOINT = '/links/';
